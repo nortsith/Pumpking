7562b56028ba5eb529085203a0f11e4208748a5e
mvpolcay <olcaymhmmet@gmail.com>
2023-01-07 19:12:05 +0300

    add sticky and bear traps functionality and movement conditions

--- Project/Assets/Code/Scripts/Traps/Trap.cs
+++ Project/Assets/Code/Scripts/Traps/Trap.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    public float slowFactor = 0.5f;
     public float duration = 5.0f;
-
-    private bool applyForce = false;
-    private bool isTriggered = false;
-    private float elapsedTime = 0.0f;
-    private Rigidbody applyForceRigidbody;
-
     public enum Option
     {
         Bear = 1,
@@ -20,35 +13,61 @@ public class Trap : MonoBehaviour
 
     public Option TrapType;
 
+    private Movement playerMovement;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMovement = FindObjectOfType<Movement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (applyForce && !isTriggered)
+
+    }
+
+    IEnumerator TrapTimer()
+    {
+        print("Trapped!");
+        timer = duration;
+        while (timer > 0)
         {
-            if (TrapType == Option.Sticky)
-            {
-                Sticky();
-            }
+            yield return new WaitForSeconds(1);
+            timer--;
         }
-       
+        
+        playerMovement.movementState = MovementState.Default;
+        print("Not Trapped!");
+
+        yield return null;
     }
 
     void Sticky()
     {
-        Vector3 originalVelocity = applyForceRigidbody.velocity;
-        applyForceRigidbody.velocity = originalVelocity * slowFactor;
+        playerMovement.movementState = MovementState.Slowed;
+        StartCoroutine(TrapTimer());
+    }
+
+    void Bear()
+    {
+        playerMovement.movementState = MovementState.Immobilize;
+        StartCoroutine(TrapTimer());
     }
     
     private void OnTriggerEnter(Collider trappedObject) {
-        print(trappedObject.gameObject.tag);
-        applyForceRigidbody = trappedObject.gameObject.GetComponent<Rigidbody>();
-        applyForce = true;
-        isTriggered = true;
+        switch (TrapType)
+        {
+            case Option.Sticky:
+                Sticky();
+                return;
+            case Option.Bear:
+                Bear();
+                return;
+            default:
+                return;
+        }
+        
     }
 }
